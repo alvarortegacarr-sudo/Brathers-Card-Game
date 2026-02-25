4b320712620eedc609b9676c45cd4e5ebf480e07
alvarortegacarr-sudo <alvarortegacarr@gmail.com>
2026-02-25 19:35:06 +0100

    Pruebas

--- js/supabase.js
+++ js/supabase.js
@@ -4,7 +4,7 @@
 
 // Your Supabase credentials
 const SUPABASE_URL = 'https://cgbwupduikywkknwwkab.supabase.co'; // Replace with your URL
-const SUPABASE_KEY = 'sb_publishable_CJvDobHTCqwvEMqR3LjMdQ_y_WC4Y-F'; // Replace with your anon key
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNnYnd1cGR1aWt5d2trbnd3a2FiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEwOTQ2NDcsImV4cCI6MjA4NjY3MDY0N30.U0Z_ejpsmtR22RRTVCvjCaRpz2ZENgUhVnGv5mXmxmE'; // Replace with your anon key
 
 // Create client
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
